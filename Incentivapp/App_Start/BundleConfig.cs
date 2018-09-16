950c2ec7eba5766fae206185f90340be8b4cdf51
Giampiero Specogna <giampi_12@hotmail.com>
2018-09-15 21:18:12 -0400

    Initial commit

---
+++ Incentivapp/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Incentivapp
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                            .Include("~/Content/bootstrap/css/bootstrap.min.css",
+                                       "~/Content/js/jquery.min.js",
+                                    "~/Content/bootstrap/js/bootstrap.min.js",
+                                    "~/Content/styles.css"));
+        }
+    }
+}
